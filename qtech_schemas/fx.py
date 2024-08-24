d2cb471f734a97ceb0f3c6333f58573b9d74c97e
IanContrerasM <ic@quantech.com.do>
2024-08-23 21:48:37 -0400

    hey2

--- qtech_schemas/fx.py
+++ qtech_schemas/fx.py
@@ -126,7 +126,7 @@ class FxAnalytics(Base):
 
     variable: Mapped[FxVariable] = relationship(back_populates='analytics')
     transform: Mapped[FxTypeTransform] = relationship(back_populates='analytics')
-    probability_fact: Mapped[List['FxProbabilityFact']] = relationship(back_populates='analytics')
+    probability_fact: Mapped[List['FxProbabilityFact']] = relationship(back_populates='analytic')
 
 class FxAnalyticsFact(Base):
     __tablename__ = 'ANALYTICS_FACT'
