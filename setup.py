dcdb8ece94c9cb6400b44112b43c3723dfe6d41d
IanContrerasM <ic@quantech.com.do>
2024-09-19 14:36:21 -0400

    update_yield

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='qtech_schemas',
-    version='2.9.15.17',
+    version='2.9.15.17.1',
     packages=['qtech_schemas'],
     description='Libreria con las tablas del schema MARKET',
     author='Jose M.',
